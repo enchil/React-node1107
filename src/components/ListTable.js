2678608f6af314660d77a8c4520c706a9411aa84
enchil <enchiliang@gmail.com>
2022-11-07 13:33:23 +0800

    使用 dayjs 列表表格

---
+++ src/components/ListTable.js
@@ -0,0 +1,34 @@
+import dayjs from "dayjs"
+
+function ListTable({rows}) {
+    console.log({rows})
+    return (<table className="table table-striped table-hover">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">姓名</th>
+        <th scope="col">手機</th>
+        <th scope="col">Email</th>
+        <th scope="col">生日</th>
+      </tr>
+    </thead>
+    {/* 把資料放進去tbody讀出來一條一條的rows */}
+    <tbody>
+{rows.map((el)=>{
+  const d = dayjs(el.birthday)
+  return(<tr key={el.sid}>
+        <td>{ el.sid }</td>
+        <td>{ el.name }</td>
+        <td>{ el.mobile }</td>
+        <td>{ el.email }</td>
+        <td>{ d.isValid() && d.format('YYYY-MM-DD') }</td>
+      </tr>)
+    
+})}
+      
+      
+    </tbody>
+  </table>)
+}
+
+export default ListTable
--- src/pages/ab-list.js
+++ src/pages/ab-list.js
@@ -2,14 +2,21 @@
 import {useEffect, useState} from 'react';
 import {AB_LIST} from './../my-config'; 
 import axios from 'axios'
+import ListTable from './../components/ListTable';
 
 function AbList() {
-  const [listData, setListData] = useState({});
+  const [listData, setListData] = useState({
+    totalRows: 0,
+    totalPages: 0,
+    perPage: 0,
+    page: 1,
+    rows: []
+  });
 
   async function getList(){
     const response = await axios.get(AB_LIST)
     console.log(response);
-    setListData(response);
+    setListData(response.data);
   }
 
   useEffect(() => {
@@ -18,7 +25,15 @@ function AbList() {
   }, []);
   console.log(1);
   return (
-      <pre>{ JSON.stringify(listData, null, 4) }</pre>
+    <>
+    <div className='container'>
+    <ListTable rows={listData.rows}/>
+    </div>
+    
+    {/* <pre>{ JSON.stringify(listData, null, 4) }</pre> */}
+
+    </>
+      
   );
 }
 
