c49275d9cff61faec31d78cba11cd8f950530138
enchil <enchiliang@gmail.com>
2022-11-10 11:12:11 +0800

    購物車功能 新增刪除

--- src/pages/Canvas1.js
+++ src/pages/Canvas1.js
@@ -1,31 +1,79 @@
-import { useRef, useEffect } from "react"
-import { productData } from "../data/product-data"
+import { useRef, useEffect, useState } from "react";
+import { productData } from "./../data/product-data";
 
-export default function Canvas1(params) {
-    const cRef = useRef()
+export default function Canvas1() {
+  const cRef = useRef();
+  const [cart, setCart] = useState([]);
 
-    useEffect(()=>{
-        const ctx = cRef.current.getContext("2d")
-        const img = new Image();
-        img.onload = ()=>{
-            ctx.drawImage(img, 0, 0)
-        }
-        img.src = "/imgs/dish.jpeg"
-    },[])
+  const cartAdd = (pid) => {
+    let p = productData.find((el) => el.id === pid);
+    if(p){
+        p = {...p};
+        p.tid = Date.now();
+        const CurrentCart = [...cart, p];//=push
+        setCart(CurrentCart);
+        console.log(CurrentCart);
+    }
+    
+  };
+  const cartRemove = (tid) => {
+    const newCart = cart.filter(el=>el.tid!==tid)
+    setCart(newCart)
+  };
 
-    return (
-        <div className="container">
-            <div className="row"> 
-                <div className="col"> 
-                { productData.map(el=>{
-                    return <img src={"./imgs/" + el.img} alt={el.name} key={el.id} width="150"></img>
-                })}
-                </div>
-            </div>
-        
-            <canvas ref={cRef} width="800" height="600" style={{border: "1px solid lightblue"}}>
+  useEffect(() => {
+    const ctx = cRef.current.getContext("2d");
 
-            </canvas>
+    const img = new Image();
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+    };
+    img.src = "/imgs/dish.jpeg";
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          {productData.map((el) => {
+            return (
+              <img
+                src={"/imgs/" + el.img}
+                alt={el.name}
+                key={el.id}
+                width="150"
+                onClick={() => {
+                  cartAdd(el.id);
+                }}
+              />
+            );
+          })}
         </div>
-    )
+      </div>
+
+      <div className="row">
+        <div className="col">
+          {cart.map((el) => {
+            return (
+              <img
+                src={"/imgs/" + el.img}
+                alt={el.name}
+                key={el.tid}
+                width="50"
+                onClick={() => {
+                  cartRemove(el.tid);
+                }}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <canvas
+        ref={cRef}
+        width="800"
+        height="600"
+        style={{ border: "1px solid blue" }}
+      ></canvas>
+    </div>
+  );
 }
