91e90ece3ba22a6103d814f38caf1d8992520203
enchil <enchiliang@gmail.com>
2022-11-10 10:17:19 +0800

    useRef() canvas imgs

--- src/pages/Canvas1.js
+++ src/pages/Canvas1.js
@@ -1,7 +1,20 @@
+import { useRef, useEffect } from "react"
+
 export default function Canvas1(params) {
+    const cRef = useRef()
+
+    useEffect(()=>{
+        const ctx = cRef.current.getContext('2d')
+        const img = new Image();
+        img.onload = ()=>{
+            ctx.drawImage(img, 0, 0)
+        }
+        img.src = '/imgs/dish.jpeg'
+    },[])
+
     return (
         <div className="container">
-            <canvas width="800" height="600" style={{border: "1px solid lightblue"}}>
+            <canvas ref={cRef} width="800" height="600" style={{border: "1px solid lightblue"}}>
 
             </canvas>
         </div>
